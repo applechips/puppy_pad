9b0975b45c3e79f762a8a01edf888d61e78691f9
Nicole <nhswong@gmail.com>
2016-11-29 17:38:43 -0800

    add aws info

---
+++ config/initializers/setup_fog.rb
@@ -0,0 +1,22 @@
+# create a connection
+# connection = Fog::Storage.new({
+#   :provider                 => 'AWS',
+#   :region                   => 'us-west-2',
+#   :aws_access_key_id        => ENV['S3_ACCESS_KEY'],
+#   :aws_secret_access_key    => ENV['S3_SECRET_ACCESS_KEY']
+# })
+#
+#
+# )
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: 'us-west-2',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+end
