692ab3dbbdf517bd4b514ae8b808eec358e62979
Nicole <nhswong@gmail.com>
2016-11-30 16:55:30 -0800

    Change S3 Credentials

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  #  storage :file
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
